b6be7cdf8bcaf10fd690e6f3c3e85eb780882d2d
ochornolutskyi <ochornolutskyi@gmail.com>
2019-07-28 22:43:41 +0300

    realese v0.5

--- controllers/hotDogs.js
+++ controllers/hotDogs.js
@@ -1,60 +1,61 @@
 var hotDogs = require('../models/hotDogs');
 
-exports.all = function (req, res) {
-    hotDogs.all(function (err, docs) {
+exports.all = function(req, res) {
+    hotDogs.all(function(err, docs) {
         if (err) {
             console.log(err);
             return res.sendStatus(500);
         }
-        res.render('index', {ar: docs});
+        // var params = { name: }
+        res.render('index', { ar: docs });
     })
 }
 
-exports.create = function(req, res){
+exports.create = function(req, res) {
     var hotDog = {
         name: req.body.name,
         description: req.body.description
     };
-    hotDogs.create(hotDog, function (err, result){
+    hotDogs.create(hotDog, function(err, result) {
         if (err) {
             console.log(err);
             return res.sendStatus(500);
-          }
-          res.redirect('..');
+        }
+        res.redirect('..');
     })
 }
-exports.edit = function (req, res) {
-    hotDogs.findByID(req.params.id, function (err, doc) {
+exports.edit = function(req, res) {
+    hotDogs.findByID(req.params.id, function(err, doc) {
         if (err) {
             console.log(err);
             return res.sendStatus(500);
         }
         var params = {
             formAction: '/update/' + doc._id + '?_method=PUT',
-            formTitle: 'Edit Hot Dog', 
-            name: doc.name, 
+            formTitle: 'Edit Hot Dog',
+            name: doc.name,
             description: doc.description,
-          };
-          res.render('edit', { item: params });
+        };
+        res.render('edit', { item: params });
 
     });
 }
-exports.update = function(req, res){
-    hotDogs.update(req.params.id, { $set: { name: req.body.name, description: req.body.description } }, function(err, result){
+exports.update = function(req, res) {
+    hotDogs.update(req.params.id, { $set: { name: req.body.name, description: req.body.description } }, function(err, result) {
         if (err) {
             console.log(err);
             return res.sendStatus(500);
-          }
-          res.redirect('..');
+        }
+        res.redirect('..');
     })
 }
 
-exports.delete = function(req, res){
-    hotDogs.delete(req.params.id, function (err, result){
+exports.delete = function(req, res) {
+    hotDogs.delete(req.params.id, function(err, result) {
         if (err) {
             console.log(err);
             return res.sendStatus(500);
-          }
-          res.redirect('..');
+        }
+        res.redirect('..');
     })
 }
